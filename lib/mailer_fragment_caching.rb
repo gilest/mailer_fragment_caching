28679103d616af42a290d87f5f0d39c3e3d863fc
Gordon McNaughton <gmcnaughton@team.curious.com>
2016-07-31 18:07:53 +0100

    Fixes 'cache' directive in mailers not working in Rails 4.2.x.
    Closes #3

--- lib/mailer_fragment_caching.rb
+++ lib/mailer_fragment_caching.rb
@@ -14,4 +14,28 @@ module MailerFragmentCaching
     perform_caching && cache_store
   end
 
+  # In Rails4, <tt>ActionController::Caching#view_cache_dependencies</tt> must
+  # also be duplicated in order for +cache+ to work.
+  # <tt>ActionView::Helpers::CacheHelper#fragment_name_with_digest</tt> relies on it.
+  if Rails::VERSION::MAJOR >= 4
+    def self.included(base)
+      base.class_eval do
+        class_attribute :_view_cache_dependencies
+        self._view_cache_dependencies = []
+        helper_method :view_cache_dependencies if respond_to?(:helper_method)
+      end
+    end
+
+    def self.view_cache_dependency(&dependency)
+      self._view_cache_dependencies += [dependency]
+    end
+
+    def view_cache_dependencies
+      self.class._view_cache_dependencies.map { |dep| instance_exec(&dep) }.compact
+    end
+
+    def instrument_fragment_cache(name, key) # :nodoc:
+      yield if block_given?
+    end
+  end
 end
